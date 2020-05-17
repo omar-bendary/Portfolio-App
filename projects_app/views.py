087c8bf4fa831284cac47cdb42844b80956d498c
unknown <omar.bendary1996@gmail.com>
2020-05-17 05:16:47 +0200

    project app added code

--- projects_app/views.py
+++ projects_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
+
+
+def index(request):
+    projects = Project.objects.all()
+    return render(request, 'index.html', {'projects': projects})
+
+
+def detail(request, pk):
+    project_detail = Project.objects.get(pk=pk)
+    return render(request, 'project_detail.html', {'project_detail': project_detail})
