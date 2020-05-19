6267eabd0cc01c5ea94d2ac0b110e7d09ba80e2a
unknown <omar.bendary1996@gmail.com>
2020-05-19 18:02:09 +0200

    Projects App Added

--- projects_app/models.py
+++ projects_app/models.py
@@ -7,4 +7,4 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FileField()
+    image = models.FilePathField(path="/img")
--- projects_app/views.py
+++ projects_app/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from .models import Project
-
-# Create your views here.
+from projects_app.models import Project
 
 
-def index(request):
+def project_index(request):
     projects = Project.objects.all()
-    return render(request, 'index.html', {'projects': projects})
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
 
 
-def detail(request, pk):
-    project_detail = Project.objects.get(pk=pk)
-    return render(request, 'project_detail.html', {'project_detail': project_detail})
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
